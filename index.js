25041e822ac44adde7e51f8915e147dac770f14f
eduardolagoeiro <eduardolagoeiro@id.uff.br>
2019-05-25 14:27:14 -0300

    users routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { users } = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use('/api/users', users);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`)
